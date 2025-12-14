5bddafc808171a0cdae5d5e5c60eb7f73c761ec3
Eugene Trocenko <trocenko1999@gmail.com>
2025-12-14 20:38:34 +0300

    исправления

--- src/components/ShopItem.jsx
+++ src/components/ShopItem.jsx
@@ -4,10 +4,8 @@ function ShopItem({ item }) {
   return (
     <div className="shop-item">
       <img src={img} alt={name} className="shop-item-img" />
-      <div className="shop-item-info">
-        <h3 className="shop-item-name">{name}</h3>
-        <p className="shop-item-color">{color}</p>
-      </div>
+      <h3 className="shop-item-name">{name}</h3>
+      <p className="shop-item-color">{color}</p>
       <span className="shop-item-price">${price}</span>
       <button className="shop-item-btn">ADD TO CART</button>
     </div>
